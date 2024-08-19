00d7c6a4b9e73bf3a2fc1a917af3782c679fa6dc
mubeenaftab <mubeenaftab66@gmail.com>
2024-08-19 19:14:30 +0500

    first commit

--- src/components/QuickAccess.jsx
+++ src/components/QuickAccess.jsx
@@ -4,19 +4,21 @@ import { FaRegCreditCard } from 'react-icons/fa';
 import { PiNotepadBold } from "react-icons/pi";
 import { IoDocumentText } from "react-icons/io5";
 import { IoMdAirplane } from "react-icons/io";
-
+import { Link } from 'react-router-dom';
 
 function QuickAccess() {
   return (
     <section className="quick-access">
       <h3>Quick Access</h3>
       <div className="buttons">
-        <button>
-          <div className="icon-container icon1">
-            <FaRegCreditCard className="icon" />
-          </div>
-          + New Expense
-        </button>
+        <Link to="/expenses-form" className='new-expense'>
+          <button>
+            <div className="icon-container icon1">
+              <FaRegCreditCard className="icon" />
+            </div>
+            + New Expense
+          </button>
+        </Link>
         <button>
           <div className="icon-container icon2">
             <PiNotepadBold className="icon" />
