889a7120183c810b0225cee5bb44238510b5a280
uzair02 <chemsa02@gmail.com>
2024-08-20 14:47:44 +0500

    added click event to the buttons

--- src/components/QuickAccess.jsx
+++ src/components/QuickAccess.jsx
@@ -4,34 +4,34 @@ import { FaRegCreditCard } from 'react-icons/fa';
 import { PiNotepadBold } from "react-icons/pi";
 import { IoDocumentText } from "react-icons/io5";
 import { IoMdAirplane } from "react-icons/io";
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function QuickAccess() {
+  const navigate = useNavigate();
+
   return (
     <section className="quick-access">
       <h3>Quick Access</h3>
       <div className="buttons">
-        <Link to="/expenses-form" className='new-expense'>
-          <button>
-            <div className="icon-container icon1">
-              <FaRegCreditCard className="icon" />
-            </div>
-            + New Expense
-          </button>
-        </Link>
-        <button>
+        <button onClick={() => navigate('/expenses-form')}>
+          <div className="icon-container icon1">
+            <FaRegCreditCard className="icon" />
+          </div>
+          + New Expense
+        </button>
+        <button onClick={() => navigate('/create-team')}>
           <div className="icon-container icon2">
             <PiNotepadBold className="icon" />
           </div>
           + Create Team
         </button>
-        <button>
+        <button onClick={() => navigate('/create-category')}>
           <div className="icon-container icon3">
             <IoDocumentText className="icon" />
           </div>
           + Create Category
         </button>
-        <button>
+        <button onClick={() => navigate('/create-team')}>
           <div className="icon-container icon4">
             <IoMdAirplane className="icon" />
           </div>
