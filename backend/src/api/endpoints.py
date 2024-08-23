986143d658e165da87b6297987c55e91dc374526
mubeenaftab <mubeenaftab66@gmail.com>
2024-08-23 17:37:44 +0500

    added user router in endpoints

--- backend/src/api/endpoints.py
+++ backend/src/api/endpoints.py
@@ -1,9 +1,5 @@
 import fastapi
-
-from src.api.routes.account import router as account_router
-from src.api.routes.authentication import router as auth_router
+from api.routes.user import router as user_router
 
 router = fastapi.APIRouter()
-
-router.include_router(router=account_router)
-router.include_router(router=auth_router)
+router.include_router(router=user_router)
