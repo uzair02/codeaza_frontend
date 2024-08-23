b8825b598ff06af26495d0ce3308a1280acca7f2
mubeenaftab <mubeenaftab66@gmail.com>
2024-08-23 17:38:46 +0500

    initializing logger

---
+++ backend/src/config/settings/logger_config.py
@@ -0,0 +1,10 @@
+import pathlib
+
+from loguru import logger
+
+# Go three folders back from the current file
+log_path = pathlib.Path(__file__).resolve().parents[3] / "app.log"
+# Configure the logger with the new path
+logger.add(
+    str(log_path), rotation="500 MB", retention="10 days", backtrace=True, diagnose=True
+)
