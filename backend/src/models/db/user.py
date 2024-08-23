fbd632ada0906a511163c9ee61f4b02d2b99c831
mubeenaftab <mubeenaftab66@gmail.com>
2024-08-23 17:39:10 +0500

    cerated user table in database

---
+++ backend/src/models/db/user.py
@@ -0,0 +1,13 @@
+import uuid
+
+from repository.database import Base
+from sqlalchemy import Boolean, Column, String
+from sqlalchemy.dialects.postgresql import UUID
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    username = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
