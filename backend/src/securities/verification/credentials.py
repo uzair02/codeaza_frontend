7c4137a52da3cf744822f8dbeed18a9ce297bc83
mubeenaftab <mubeenaftab66@gmail.com>
2024-08-23 17:43:16 +0500

    func for getting current user after authentication

---
+++ backend/src/securities/verification/credentials.py
@@ -0,0 +1,30 @@
+from config.settings.base import config
+from fastapi import Depends, HTTPException, status
+from jose import JWTError, jwt
+from models.db.user import User
+from models.schemas.user import TokenData
+from repository.database import get_db
+from security.hashing.hash import oauth2_scheme
+from sqlalchemy.orm import Session
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, config.SECRET_KEY, algorithms=[config.ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = db.query(User).filter(User.username == token_data.username).first()
+    if user is None:
+        raise credentials_exception
+    return user
