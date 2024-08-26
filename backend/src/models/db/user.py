fc81589317ee0eeac68310ae13f3608a7f74c59c
mubeenaftab <mubeenaftab66@gmail.com>
2024-08-26 17:36:11 +0500

    added docstring for class

--- backend/src/models/db/user.py
+++ backend/src/models/db/user.py
@@ -7,8 +7,23 @@ from repository.database import Base
 
 
 class User(Base):
+    """
+    Represents a user in the system.
+
+    Attributes:
+        user_id (UUID): Unique identifier for the user, automatically generated.
+        username (str): The username of the user. Must be unique and cannot be null.
+        hashed_password (str): The hashed password for the user. Cannot be null.
+        is_active (bool): Flag indicating whether the user account is active. Defaults to True.
+
+    Table:
+        - `users`: The table name in the database where user information is stored.
+
+    This class is mapped to the `users` table in the database and is used to manage user-related data.
+    """
+
     __tablename__ = "users"
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     username = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
