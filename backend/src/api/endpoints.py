bc3ad3fe57079ad82584705faa2d517830d02fdb
mubeenaftab <mubeenaftab66@gmail.com>
2024-08-28 16:40:15 +0500

    added endpoint router for logs

--- backend/src/api/endpoints.py
+++ backend/src/api/endpoints.py
@@ -1,6 +1,8 @@
 import fastapi
 
 from api.routes.user import router as user_router
+from api.routes.user_logs import router as user_logs_router
 
 router = fastapi.APIRouter()
 router.include_router(router=user_router)
+router.include_router(router=user_logs_router)
