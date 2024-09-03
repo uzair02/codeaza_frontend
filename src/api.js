f961151d5c392ec36c6b0bb4e12180bd3ade3b6a
mubeenaftab <mubeenaftab66@gmail.com>
2024-09-03 13:49:19 +0500

    authorization route

--- src/api.js
+++ src/api.js
@@ -1,13 +1,27 @@
 import axios from 'axios';
 
-const BaseUrl = 'http://localhost:8000';
+const api = axios.create({
+  baseURL: 'http://localhost:8000',
+});
 
-export async function fetchLast5MonthsSummary() {
-    try {
-      const response = await axios.get(`${BASE_URL}/expenses/last-5-months-summary`); // Adjust the endpoint as necessary
-      return response.data;
-    } catch (error) {
-      console.error('Error fetching last 5 months summary:', error);
-      throw error;
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
     }
-  }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+
+// export async function fetchLast5MonthsSummary() {
+//     try {
+//       const response = await axios.get(`${BASE_URL}/expenses/last-5-months-summary`); // Adjust the endpoint as necessary
+//       return response.data;
+//     } catch (error) {
+//       console.error('Error fetching last 5 months summary:', error);
+//       throw error;
+//     }
+//   }
