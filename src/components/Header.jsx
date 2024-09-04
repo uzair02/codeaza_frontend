cae64c65ac12ddc7968bc8f49d46752cff8b51d2
mubeenaftab <mubeenaftab66@gmail.com>
2024-09-04 16:09:45 +0500

    implemented getting year logic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './css/Header.css';
+import axios from 'axios';
 
-function Header() {
+function Header({ selectedYear, onYearChange }) {
+  const [years, setYears] = React.useState([]);
+
+  const handleYearChange = (event) => {
+    onYearChange(parseInt(event.target.value)); // Notify parent of the selected year
+  };
+
+  useEffect(() => {
+    const fetchAvailableYears = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/expenses/available-years');
+        if (Array.isArray(response.data)) {
+          setYears(response.data);
+        } else {
+          console.error("Unexpected response format:", response.data);
+        }
+      } catch (error) {
+        console.error("Error fetching available years:", error);
+      }
+    };
+
+    fetchAvailableYears();
+  }, []);
+
   return (
     <header className="content-header">
-      <select className="year-dropdown">
-        <option value="year" selected>Year</option>
-        <option value="2023">2023</option>
-        <option value="2022">2022</option>
-        <option value="2021">2021</option>
+      <select className="year-dropdown" value={selectedYear || ''} onChange={handleYearChange}>
+        <option value="">Year</option>
+        {years.map((year) => (
+          <option key={year} value={year}>{year}</option>
+        ))}
       </select>
     </header>
   );
